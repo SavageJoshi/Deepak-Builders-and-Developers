3b05965577c8fee09171debc209fdb834248e3f7
Sarvesh Joshi <sarveshmayjoshi@gmail.com>
2024-03-05 23:58:46 +0530

    commit_projectsPage-cardsDraft-1

--- pages/ProjectsPage/projectsPage.js
+++ pages/ProjectsPage/projectsPage.js
@@ -48,3 +48,91 @@ dropdown.addEventListener('click', (event) => {
     button.setAttribute('selected', ''); // Add the attribute
   }
 });
+
+
+
+
+// Project Cards
+
+// horizontal scroll
+let isDown = false;
+let startX;
+let scrollLeft;
+
+const projectSlider = document.querySelector('.projectscard-container');
+
+projectSlider.addEventListener('mouseenter', () => {
+  document.body.style.overflow = 'hidden';
+});
+
+projectSlider.addEventListener('mouseleave', () => {
+  document.body.style.overflow = 'auto';
+});
+
+projectSlider.addEventListener('wheel', function (e) {
+  e.preventDefault();
+  gsap.to(projectSlider, {
+    duration: 1,
+    scrollTo: { x: projectSlider.scrollLeft + e.deltaY * 5 },
+  });
+}, { passive: false });
+
+projectSlider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX;
+  scrollLeft = projectSlider.scrollLeft;
+});
+
+projectSlider.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+projectSlider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+projectSlider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX;
+  const walk = x - startX;
+  projectSlider.scrollLeft = scrollLeft - walk;
+});
+
+// card effects
+
+// Function to roll numbers using GSAP
+const rollingNumbers = (target, value) => {
+  gsap.to(target, {
+    duration: 0.8,
+    innerHTML: value.toString(),
+    roundProps: "innerHTML",
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const projectCard = document.querySelector(".projectCard");
+  const card2 = document.querySelector(".card2");
+  const overlayNumber = document.querySelector(".overlay-number");
+
+  projectCard.addEventListener("mouseenter", function () {
+    rollingNumbers("#prj1 h1", 98);
+  });
+  projectCard.addEventListener("mouseleave", function () {
+    rollingNumbers("#prj1 h1", 0);
+  });
+
+  card2.addEventListener("mouseenter", function () {
+    rollingNumbers("#prj2 h1", 120);
+  });
+  card2.addEventListener("mouseleave", function () {
+    rollingNumbers("#prj2 h1", 0);
+  });
+});
+
+
+
+
+
+
+
