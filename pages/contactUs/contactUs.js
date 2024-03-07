0ba5188ac5eff531ea760a63d24aa9cb79d52598
Sarvesh Joshi <sarveshmayjoshi@gmail.com>
2024-03-07 21:09:58 +0530

    commit_contactUs-frontend

---
+++ pages/contactUs/contactUs.js
@@ -0,0 +1,31 @@
+
+// Navbar
+
+
+const app = (() => {
+    let navContainer;
+    let menu;
+    let menuItems;
+  
+    const init = () => {
+        navContainer = document.querySelector('.nav-container');
+        menu = document.querySelector('.menu-icon');
+        menuItems = document.querySelectorAll('.nav__list-item');
+  
+        applyListeners();
+    }
+  
+    const applyListeners = () => {
+        menu.addEventListener('click', () => toggleClass(navContainer, 'nav-active'));
+    }
+  
+    const toggleClass = (element, stringClass) => {
+        if(element.classList.contains(stringClass))
+            element.classList.remove(stringClass);
+        else
+            element.classList.add(stringClass);
+    }
+  
+    init();
+  })();
+  
